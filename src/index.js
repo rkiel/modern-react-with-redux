39feaa0a30d3924447a4391177d39aa1d6af2642
rkiel <cygnus@pobox.com>
2016-03-19 09:13:06 -0400

    search-bar: hardcoded youtube search

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
+
+import YTAPI from './config/youtube_api';
 import SearchBar from './components/search_bar';
+
+YTSearch({ key: YTAPI.key, term: 'surfboards' }, (data) => {
+  console.log(data);
+});
 
 const App = () => {
   return (
