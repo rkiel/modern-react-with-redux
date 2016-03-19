3b9acc3330f4927e835e0369badc2fcf118ea47e
rkiel <cygnus@pobox.com>
2016-03-19 11:43:40 -0400

    search-bar: add on select function

--- src/components/video_list.js
+++ src/components/video_list.js
@@ -2,7 +2,14 @@ import React from 'react';
 import VideoListItem from './video_list_item';
 
 const VideoList = (props) => {
-  const videoItems = props.videos.map(x => <VideoListItem key={x.etag} video={x} />);
+  const videoItems = props.videos.map(x => {
+    return (
+        <VideoListItem
+          key={x.etag}
+          video={x}
+          onVideoSelect={props.onVideoSelect} />
+    );
+  });
 
   return (
     <ul className="col-md-4 list-group">
