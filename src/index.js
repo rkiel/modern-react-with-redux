aa7c0700aef431b5329f5c335b71802fbcea70a0
rkiel <cygnus@pobox.com>
2016-03-19 10:00:25 -0400

    search-bar: move intial search to constructor

--- src/index.js
+++ src/index.js
@@ -4,12 +4,16 @@ import YTSearch from 'youtube-api-search';
 
 import YTAPI from './config/youtube_api';
 import SearchBar from './components/search_bar';
-
-YTSearch({ key: YTAPI.key, term: 'surfboards' }, (data) => {
-  console.log(data);
-});
-
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { videos: [] };
+
+    YTSearch({ key: YTAPI.key, term: 'surfboards' }, (videos) => {
+      this.setState({ videos });
+    });
+  }
+
   render() {
     return (
       <div>
